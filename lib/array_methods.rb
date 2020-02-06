94a416fe0df68a710c75e297106fcc4f6b848b71
Ryan Seit <ryan@ryanseit.com>
2020-02-06 23:24:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,14 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times { |index|
+    !!array[index] == value_to_find
+    puts index
+  }
 end
 
 def find_max_value(array)
-  # Add your solution here
+
 end
 
 def find_min_value(array)
-  # Add your solution here
+
 end
