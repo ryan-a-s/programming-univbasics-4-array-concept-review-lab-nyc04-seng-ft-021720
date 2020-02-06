804660e2913cf343c68586edea2a458485240a13
Ryan Seit <ryan@ryanseit.com>
2020-02-06 23:45:05 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,19 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times { |index|
+    return array.index(value_to_find)
+  }
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.length.times { |index|
+    array.sort!
+    return array[-1]
+  }
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.length.times { |index|
+    array.sort!
+    return array[0]
+  }
 end
